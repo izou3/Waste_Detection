bc9583de29868c7b1156226a97de956a5998bdd2
Ivan Zou <izou3@gatech.edu>
2021-11-27 19:47:40 -0500

    Added Training Images

--- tests/KNNTracker.py
+++ tests/KNNTracker.py
@@ -1,5 +1,6 @@
 import cv2
 import root
+import numpy as np
 from videos import env
 
 OPENCV_MAJOR_VERSION = int(cv2.__version__.split('.')[0])
@@ -9,9 +10,20 @@ bg_subtractor = cv2.createBackgroundSubtractorKNN(detectShadows=True)
 erode_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 5))
 dilate_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (17, 11))
 
-path = env._ABSOLUTE_PATH + r"\TestVideo5.h264"
+# path = env._ABSOLUTE_PATH + r"\TestVideo5.h264"
+path = r"D:\West_Village_Waste_Footage\11-16-2021\Front-Trash-Can\2021\11\16\TestVideoY.h264"
 cap = cv2.VideoCapture(path)
 success, frame = cap.read()
+
+print("Detecting circle")
+# Detect the circle
+gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+gray = cv2.medianBlur(gray, 5)
+rows = gray.shape[0]
+circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, rows / 8,
+                               param1=100, param2=30,
+                               minRadius=1, maxRadius=0)
+
 while success:
 
     fg_mask = bg_subtractor.apply(frame)
@@ -33,13 +45,22 @@ while success:
                                              cv2.CHAIN_APPROX_SIMPLE)
 
     for c in contours:
-        if cv2.contourArea(c) > 1000:
+        if cv2.contourArea(c) > 5000:
             x, y, w, h = cv2.boundingRect(c)
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 255, 0), 2)
+            if (y != 0):
+                cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 255, 0), 2)  # last 2 args are color and border width of rectangle 
 
-    cv2.imshow('knn', fg_mask)
-    cv2.imshow('thresh', thresh)
-    cv2.imshow('detection', frame)
+    if circles is not None:
+        circles = np.uint16(np.around(circles))
+        for i in circles[0, :]:
+            center = (i[0], i[1])
+            radius = i[2]
+            cv2.circle(frame, center, radius, (255, 0, 255), 3)
+
+    # cv2.imshow('knn', fg_mask)
+    # cv2.imshow('thresh', thresh)
+    resize = cv2.resize(frame, (600, 400))
+    cv2.imshow('detection', resize)
 
     k = cv2.waitKey(30)
     if k == 27:  # Escape
